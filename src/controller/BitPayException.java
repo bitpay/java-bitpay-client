b9582e0cdc2265df18fd6f66e5cffefb2f6abac5
Rich Morgan <ionux@users.noreply.github.com>
2015-03-12 21:17:49 -0400

    Missed a tab

--- src/controller/BitPayException.java
+++ src/controller/BitPayException.java
@@ -4,7 +4,7 @@ public class BitPayException extends Exception {
 
     public BitPayException(String message) {
         super(message);
-	}
+    }
 
     private static final long serialVersionUID = 1L;
 }
