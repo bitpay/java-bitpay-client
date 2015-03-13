b86665bd8e2b1b3a6e73a06cc6dc987ece1a335a
Rich Morgan <ionux@users.noreply.github.com>
2015-03-12 21:17:14 -0400

    Cleaned up source formatting to match
    how we're formatting our other libraries.

--- src/controller/BitPayException.java
+++ src/controller/BitPayException.java
@@ -2,9 +2,9 @@ package controller;
 
 public class BitPayException extends Exception {
 
-	public BitPayException(String message) {
-		super(message);
+    public BitPayException(String message) {
+        super(message);
 	}
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 }
