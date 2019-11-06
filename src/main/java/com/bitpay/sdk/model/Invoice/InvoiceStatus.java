c971a7c8b7cdbd6cbb509f6d60115a690f1cd8b5
Antonio Buedo <sales-engineering@bitpay.com>
2019-11-06 11:03:32 +0100

    - Invoice model updated - Guide updated

--- src/main/java/com/bitpay/sdk/model/Invoice/InvoiceStatus.java
+++ src/main/java/com/bitpay/sdk/model/Invoice/InvoiceStatus.java
@@ -2,10 +2,10 @@ package com.bitpay.sdk.model.Invoice;
 
 public class InvoiceStatus {
 
-    public static final String New        = "new";
-    public static final String Funded     = "funded";
-    public static final String Processing = "processing";
-    public static final String Complete   = "complete";
-    public static final String Failed     = "failed";
-    public static final String Cancelled  = "cancelled";
+    public static final String New       = "new";
+    public static final String Paid      = "paid";
+    public static final String Confirmed = "confirmed";
+    public static final String Complete  = "complete";
+    public static final String Expired   = "expired";
+    public static final String Invalid   = "invalid";
 }
