c49e7f3d80734d1e14f63a760ca760c6acb41544
Antonio Buedo <anbugal@gmail.com>
2020-11-10 18:48:36 +0100

    - Tests refactor

--- src/test/java/test/BitPayTestPayouts.java
+++ src/test/java/test/BitPayTestPayouts.java
@@ -20,17 +20,15 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class BitPayTest3 {
+public class BitPayTestPayouts {
 
     private static final BitPayLogger _log = new BitPayLogger(BitPayLogger.DEBUG);
-    private static final String clientName = "BitPay Java Library Tester3";
     private static String pairingCode;
     private static URI myKeyFile;
     private Client bitpay;
 
     @BeforeClass
     public static void setUpOneTime() throws InterruptedException, IOException, BitPayException, URISyntaxException {
-        boolean dumpOut = false;
 
 //        Client bitpay = new Client("BitPay.config.json");
         Client bitpay = new Client(
@@ -43,34 +41,7 @@ public class BitPayTest3 {
                 }},
                 null
         );
-
-        // Authorize this client for use with a BitPay merchant account.  This client requires a
-        // PAYROLL facades.
-        if (!bitpay.tokenExist(Facade.Payroll)) {
-            // Get PAYROLL facade authorization.
-            // Obtain a pairingCode from your BitPay account administrator.  When the pairingCode
-            // is created by your administrator it is assigned a facade.  To generate payout batches a
-            // PAYROLL facade is required.
-
-            // As an alternative to this client outputting a pairing code, the BitPay account owner
-            // may interactively generate a pairing code via the BitPay merchant dashboard at
-            // https://[test].bitpay.com/dashboard/merchant/api-tokens.  This client can subsequently
-            // accept the pairing code using the following call.
-
-            // bitpay.authorizeClient(pairingCode);
-
-            pairingCode = bitpay.requestClientAuthorization(Facade.Payroll);
-
-            // Signal the device operator that this client needs to be paired with a merchant account.
-            _log.info("Client is requesting PAYROLL facade access. Go to " + Env.TestUrl + " and pair this client with your merchant account using the pairing code: " + pairingCode);
-            dumpOut = true;
-            //we already failed to authorize for a PAYROLL token, therefore we must sleep a bit to try to authorize for any other facade (rate limiter on the api side)
-            Thread.sleep(10000);
-        }
-
-        if (dumpOut) {
-            throw new BitPayException("Error: client is not authorized.");
-        }
+        bitpay.setLoggerLevel(BitPayLogger.DEBUG);
     }
 
     @Before
