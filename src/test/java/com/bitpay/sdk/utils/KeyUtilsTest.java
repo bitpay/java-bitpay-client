99fb468c3355d45934d65112f2993f31210ce7df
Robert Brodie <r.brodie@sumoheavy.com>
2022-08-08 23:48:17 -0400

    SP-34 Add test for constructor and bytesToHex()

--- src/test/java/com/bitpay/sdk/utils/KeyUtilsTest.java
+++ src/test/java/com/bitpay/sdk/utils/KeyUtilsTest.java
@@ -10,6 +10,12 @@ import static org.junit.Assert.*;
 public class KeyUtilsTest {
 
   @Test
+  public void whenInstantiated_thenReturnKeyUtils() {
+    KeyUtils keyUtils = new KeyUtils();
+    assertNotNull(keyUtils);
+  }
+
+  @Test
   public void whenCreateEcKey_thenReturnKey() {
     ECKey actualKey = KeyUtils.createEcKey();
     assertNotNull(actualKey);
@@ -17,9 +23,17 @@ public class KeyUtilsTest {
 
   @Test
   public void givenHex_whenConvertToBytes_thenReturnBytes() {
-    String hex = "0123456789ABCDEF";
+    String hex = "0123456789abcdef";
     byte[] expectedBytes = { 1, 35, 69, 103, -119, -85, -51, -17 };
     byte[] actualBytes = KeyUtils.hexToBytes(hex);
     assertArrayEquals(expectedBytes, actualBytes);
+  }
+
+  @Test
+  public void givenBytes_whenConvertToHex_thenReturnHex() {
+    byte[] bytes = { 1, 35, 69, 103, -119, -85, -51, -17 };
+    String expectedHex = "0123456789abcdef";
+    String actualHex = KeyUtils.bytesToHex(bytes);
+    assertEquals(expectedHex, actualHex);
   }
 }
