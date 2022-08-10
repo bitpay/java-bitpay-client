830ae655858f6cd4d1c67329491029e84e8ad084
Robert Brodie <r.brodie@sumoheavy.com>
2022-08-10 14:50:58 -0400

    SP-59 Add coverage for Policy model

---
+++ src/test/java/com/bitpay/sdk/model/PolicyTest.java
@@ -0,0 +1,35 @@
+package com.bitpay.sdk.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PolicyTest {
+  @Test
+  public void givenSetPolicyFields_thenReturnGetPolicyFields() {
+    Policy policy = new Policy();
+    List<String> params = new ArrayList<String>();
+    
+    String expectedSIN = "Tf1XQVBRPxVeQEQXUb7NTtcoB1qbAzGYBQ9";
+    params.add(expectedSIN);
+
+    String expectedPolicy = "sin";
+    String expectedMethod = "requireSin";
+    List<String> expectedParams = params;
+
+    policy.setPolicy(expectedPolicy);
+    policy.setMethod(expectedMethod);
+    policy.setParams(expectedParams);
+
+    String actualPolicy = policy.getPolicy();
+    String actualMethod = policy.getMethod();
+    List<String> actualParams = policy.getParams();
+
+    assertEquals(actualPolicy, expectedPolicy);
+    assertEquals(actualMethod, expectedMethod);
+    assertEquals(actualParams, expectedParams);
+  }
+}
