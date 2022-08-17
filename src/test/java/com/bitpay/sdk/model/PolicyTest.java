7bec130802b438d048ac565a13716371c42c215b
Robert Brodie <r.brodie@sumoheavy.com>
2022-08-17 13:36:00 -0400

    SP-104 Selectively import classes

--- src/test/java/com/bitpay/sdk/model/PolicyTest.java
+++ src/test/java/com/bitpay/sdk/model/PolicyTest.java
@@ -2,7 +2,7 @@ package com.bitpay.sdk.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/com/bitpay/sdk/model/Rate/RateTest.java
+++ src/test/java/com/bitpay/sdk/model/Rate/RateTest.java
@@ -2,7 +2,7 @@ package com.bitpay.sdk.model.Rate;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class RateTest {
   @Test
