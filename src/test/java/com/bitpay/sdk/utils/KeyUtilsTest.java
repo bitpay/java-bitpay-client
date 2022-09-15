21c698634a11b98615222f493661bd29d3bf462e
Robert Brodie <r.brodie@sumoheavy.com>
2022-09-14 20:47:49 -0400

    SP-193 Update KeyUtilsTest

--- src/test/java/com/bitpay/sdk/utils/KeyUtilsTest.java
+++ src/test/java/com/bitpay/sdk/utils/KeyUtilsTest.java
@@ -13,37 +13,44 @@ import org.junit.jupiter.api.Test;
 public class KeyUtilsTest {
 
   @Test
-  public void whenInstantiated_thenReturnKeyUtils() {
+  public void testKeyUtils() {
     KeyUtils keyUtils = new KeyUtils();
     assertNotNull(keyUtils);
   }
 
   @Test
-  public void whenECKeyCreated_thenReturnBitcoinJECKey() {
+  public void testCreateEcKeyClass() {
     ECKey expectedClass = new ECKey();
+
     ECKey actualClass = KeyUtils.createEcKey();
+
     assertEquals(expectedClass.getClass(), actualClass.getClass());
   }
 
   @Test
-  public void whenCreateEcKey_thenReturnKey() {
+  public void testCreateEcKeyNotNull() {
     ECKey actualKey = KeyUtils.createEcKey();
+
     assertNotNull(actualKey);
   }
 
   @Test
-  public void givenHex_whenConvertToBytes_thenReturnBytes() {
+  public void testHexToBytes() {
     String hex = "0123456789abcdef";
     byte[] expectedBytes = { 1, 35, 69, 103, -119, -85, -51, -17 };
+    
     byte[] actualBytes = KeyUtils.hexToBytes(hex);
+    
     assertArrayEquals(expectedBytes, actualBytes);
   }
 
   @Test
-  public void givenBytes_whenConvertToHex_thenReturnHex() {
+  public void testBytesToHex() {
     byte[] bytes = { 1, 35, 69, 103, -119, -85, -51, -17 };
     String expectedHex = "0123456789abcdef";
+    
     String actualHex = KeyUtils.bytesToHex(bytes);
+    
     assertEquals(expectedHex, actualHex);
   }
 }
