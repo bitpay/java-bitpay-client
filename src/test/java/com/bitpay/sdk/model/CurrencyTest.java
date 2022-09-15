63b0decb2eef43b6316d6e434a60c54a4ea81ff7
Robert Brodie <r.brodie@sumoheavy.com>
2022-09-14 20:39:42 -0400

    SP-193 Update CurrencyTest

--- src/test/java/com/bitpay/sdk/model/CurrencyTest.java
+++ src/test/java/com/bitpay/sdk/model/CurrencyTest.java
@@ -7,16 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CurrencyTest {
   @Test
-  public void givenValidCurrencyString_thenReturnTrue() {
+  public void testGetIsValidTrue() {
     String currency = "BTC";
     Boolean isValid = Currency.isValid(currency);
+    
     assertTrue(isValid);
   }
 
   @Test
-  public void givenInvalidCurrencyString_thenReturnFalse() {
+  public void testGetIsValidFalse() {
     String currency = "FAKECURRENCY";
     Boolean isValid = Currency.isValid(currency);
+    
     assertFalse(isValid);
   }
 }
