d4ac3835fd3364d4c678d1e55287e787cff46081
Marcin Warzybok <m.warzybok@sumoheavy.com>
2022-10-25 12:48:25 +0200

    SP-31 Add JavaDoc to Wallet model classes

--- src/main/java/com/bitpay/sdk/model/Wallet/Currencies.java
+++ src/main/java/com/bitpay/sdk/model/Wallet/Currencies.java
@@ -1,9 +1,17 @@
+/*
+ * Copyright (c) 2019 BitPay
+ */
 package com.bitpay.sdk.model.Wallet;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The type Currencies.
+ *
+ * @see <a href="https://bitpay.com/api/#rest-api-resources-wallets">Wallets</a>
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Currencies {
     private String _code;
@@ -13,60 +21,125 @@ public class Currencies {
     private CurrencyQr _qr;
     private String _image;
 
+    /**
+     * Instantiates a new Currencies.
+     */
     public Currencies() {
     }
 
+    /**
+     * Gets identifying code for the currency.
+     *
+     * @return the code
+     */
     @JsonIgnore
     public String getCode() {
         return _code;
     }
 
+    /**
+     * Sets identifying code for the currency.
+     *
+     * @param code the code
+     */
     @JsonProperty("code")
     public void setCode(String code) {
         this._code = code;
     }
 
+    /**
+     * Gets P2P - it indicates that this is a peer to peer (p2p) payment method (as opposed to payment protocol).
+     *
+     * @return the p2p
+     */
     @JsonIgnore
     public Boolean getP2p() {
         return _p2p;
     }
 
+    /**
+     * Sets P2P - it indicates that this is a peer to peer (p2p) payment method (as opposed to payment protocol).
+     *
+     * @param p2p the p2p
+     */
     @JsonProperty("p2p")
     public void setP2p(Boolean p2p) {
         this._p2p = p2p;
     }
 
+    /**
+     * Gets dapp browser - it indicates that this payment method operates via a browser plugin
+     * interacting with the invoice.
+     *
+     * @return the dapp browser
+     */
     @JsonIgnore
     public Boolean getDappBrowser() {
         return _dappBrowser;
     }
 
+    /**
+     * Sets dapp browser - it indicates that this payment method operates via a browser plugin
+     * interacting with the invoice.
+     *
+     * @param dappBrowser the dapp browser
+     */
     @JsonProperty("dappBrowser")
     public void setDappBrowser(Boolean dappBrowser) {
         this._dappBrowser = dappBrowser;
     }
 
+    /**
+     * Gets pay pro. Whether or not BitPay Payment Protocol is supported on this particular currency option.
+     *
+     * @return the pay pro
+     */
     @JsonIgnore
     public Boolean getPayPro() { return _payPro; }
 
+    /**
+     * Sets pay pro. Whether or not BitPay Payment Protocol is supported on this particular currency option.
+     *
+     * @param payPro the pay pro
+     */
     @JsonProperty("payPro")
     public void setPayPro(Boolean payPro) {
         this._payPro = payPro;
     }
 
+    /**
+     * Gets QR. Object containing QR code related information to show for this payment method.
+     *
+     * @return the qr
+     */
     @JsonIgnore
     public CurrencyQr getQr() { return _qr; }
 
+    /**
+     * Sets qr. Object containing QR code related information to show for this payment method.
+     *
+     * @param qr the QR
+     */
     @JsonProperty("qr")
     public void setQr(CurrencyQr qr) {
         this._qr = qr;
     }
-    
+
+    /**
+     * Gets URL that displays wallet avatar image.
+     *
+     * @return the image
+     */
     @JsonIgnore
     public String getImage() {
         return _image;
     }
 
+    /**
+     * Sets URL that displays wallet avatar image.
+     *
+     * @param image the image
+     */
     @JsonProperty("image")
     public void setImage(String image) {
         this._image = image;
--- src/main/java/com/bitpay/sdk/model/Wallet/Wallet.java
+++ src/main/java/com/bitpay/sdk/model/Wallet/Wallet.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (c) 2019 BitPay
+ */
 package com.bitpay.sdk.model.Wallet;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -6,6 +9,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
+/**
+ * The type Wallet. Currencies are fiat currencies supported by BitPay.
+ *
+ * @see <a href="https://bitpay.com/api/#rest-api-resources-wallets">Wallets</a>
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Wallet {
     private String _key;
@@ -15,60 +23,123 @@ public class Wallet {
     private ArrayList<Currencies> _currencies;
     private String _image;
 
+    /**
+     * Instantiates a new Wallet.
+     */
     public Wallet() {
     }
 
+    /**
+     * Gets a unique identifier for the wallet.
+     *
+     * @return the key
+     */
     @JsonIgnore
     public String getKey() {
         return _key;
     }
 
+    /**
+     * Sets a unique identifier for the wallet.
+     *
+     * @param key the key
+     */
     @JsonProperty("key")
     public void setKey(String key) {
         this._key = key;
     }
 
+    /**
+     * Gets human readable display name for the wallet.
+     *
+     * @return the display name
+     */
     @JsonIgnore
     public String getDisplayName() {
         return _displayName;
     }
 
+    /**
+     * Sets human readable display name for the wallet.
+     *
+     * @param displayName the display name
+     */
     @JsonProperty("displayName")
     public void setDisplayName(String displayName) {
         this._displayName = displayName;
     }
 
+    /**
+     * Gets filename of a wallet graphic (not fully qualified).
+     *
+     * @return the avatar
+     */
     @JsonIgnore
     public String getAvatar() {
         return _avatar;
     }
 
+    /**
+     * Sets filename of a wallet graphic (not fully qualified).
+     *
+     * @param avatar the avatar
+     */
     @JsonProperty("avatar")
     public void setAvatar(String avatar) {
         this._avatar = avatar;
     }
 
+    /**
+     * Gets pay pro. Whether or not the wallet supports ANY BitPay Payment Protocol options.
+     *
+     * @return the pay pro
+     */
     @JsonIgnore
     public Boolean getPayPro() { return _payPro; }
 
+    /**
+     * Sets pay pro. Whether or not the wallet supports ANY BitPay Payment Protocol options.
+     *
+     * @param payPro the pay pro
+     */
     @JsonProperty("payPro")
     public void setPayPro(Boolean payPro) {
         this._payPro = payPro;
     }
 
+    /**
+     * Gets currencies. Details of what currencies support payments for this wallet.
+     *
+     * @return the currencies
+     */
     @JsonIgnore
     public ArrayList<Currencies> getCurrencies() { return _currencies; }
 
+    /**
+     * Sets currencies. Details of what currencies support payments for this wallet.
+     *
+     * @param currencies the currencies
+     */
     @JsonProperty("currencies")
     public void setCurrencies(ArrayList<Currencies> currencies) {
         this._currencies = currencies;
     }
-    
+
+    /**
+     * Gets image. URL that displays currency image.
+     *
+     * @return the image
+     */
     @JsonIgnore
     public String getImage() {
         return _image;
     }
 
+    /**
+     * Sets image. URL that displays currency image.
+     *
+     * @param image the image
+     */
     @JsonProperty("image")
     public void setImage(String image) {
         this._image = image;
