1b6550eaad66d999458615cf79ea334ac444f72b
Marcin Warzybok <m.warzybok@sumoheavy.com>
2022-11-03 11:50:31 +0100

    SP-260 Failing DateDeserializer test

--- src/test/java/com/bitpay/sdk/util/DateDeserializerTest.java
+++ src/test/java/com/bitpay/sdk/util/DateDeserializerTest.java
@@ -16,10 +16,11 @@ public class DateDeserializerTest {
     @Test
     public void it_should_deserialize_date() throws IOException {
         // given
+        System.setProperty("user.timezone", "UTC");
         DateDeserializer testedClass = new DateDeserializer();
         JsonParser jsonParser = Mockito.mock(JsonParser.class);
         DeserializationContext deserializationContext = Mockito.mock(DeserializationContext.class);
-        Mockito.when(jsonParser.getText()).thenReturn("1977-05-24T20:07:03.232Z");
+        Mockito.when(jsonParser.getText()).thenReturn("1977-05-24T18:07:03.232Z");
 
         // when
         Long result = testedClass.deserialize(jsonParser, deserializationContext);
