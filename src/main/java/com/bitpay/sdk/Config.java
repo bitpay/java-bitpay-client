06c2e7fb61005dda5d6e7731cd7fc7c7afd1cb0a
Robert Brodie <r.brodie@sumoheavy.com>
2023-08-02 20:13:14 -0400

    SP-628 Bump BITPAY_PLUGIN_INFO to BitPay_Java_Client_v9.0.2

--- src/main/java/com/bitpay/sdk/Config.java
+++ src/main/java/com/bitpay/sdk/Config.java
@@ -34,7 +34,7 @@ public class Config {
     /**
      * BitPay Plugin Info Version.
      */
-    public static final String BITPAY_PLUGIN_INFO = "BitPay_Java_Client_v9.0.1";
+    public static final String BITPAY_PLUGIN_INFO = "BitPay_Java_Client_v9.0.2";
     /**
      * BitPay Api Frame.
      */
