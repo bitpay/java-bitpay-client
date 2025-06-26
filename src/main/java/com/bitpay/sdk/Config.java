9d05d0983b126c57944c182045ef2b0cee026403
Robert Brodie <r.brodie@sumoheavy.com>
2025-06-26 16:08:28 -0400

    #324 Prepare v10.2.1

--- src/main/java/com/bitpay/sdk/Config.java
+++ src/main/java/com/bitpay/sdk/Config.java
@@ -34,7 +34,7 @@ public class Config {
     /**
      * BitPay Plugin Info Version.
      */
-    public static final String BITPAY_PLUGIN_INFO = "BitPay_Java_Client_v10.2.0";
+    public static final String BITPAY_PLUGIN_INFO = "BitPay_Java_Client_v10.2.1";
     /**
      * BitPay Api Frame.
      */
