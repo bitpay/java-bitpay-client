03990d1ecb699caa47d7618982e5023c70895b85
Robert Brodie <r.brodie@sumoheavy.com>
2026-01-19 11:31:29 -0500

    Bump version to 10.2.5, add Java 25

--- src/main/java/com/bitpay/sdk/Config.java
+++ src/main/java/com/bitpay/sdk/Config.java
@@ -34,7 +34,7 @@ public class Config {
     /**
      * BitPay Plugin Info Version.
      */
-    public static final String BITPAY_PLUGIN_INFO = "BitPay_Java_Client_v10.2.4";
+    public static final String BITPAY_PLUGIN_INFO = "BitPay_Java_Client_v10.2.5";
     /**
      * BitPay Api Frame.
      */
